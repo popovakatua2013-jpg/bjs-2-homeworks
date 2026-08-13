d3736816d74a41e9d0af285cf0440310d133571e
popovakatua2013-jpg <popovakatua2013@yandex.ru>
2026-08-13 19:29:56 +0300

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,67 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.intervalId = null;
+  }
+
+  addClock(time, callback) {
+    if (!time || !callback) {
+      throw new Error("Отсутствуют обязательные аргументы");
+    }
+
+    const alreadyExists = this.alarmCollection.some(alarm => alarm.time === time);
+    if (alreadyExists) {
+      console.warn("Уже присутствует звонок на это же время");
+    }
+
+    this.alarmCollection.push({
+      callback: callback,
+      time: time,
+      canCall: true
+    });
+  }
+
+  removeClock(time) {
+    this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+  }
+
+  getCurrentFormattedTime() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    return hours + ":" + minutes;
+  }
+
+  start() {
+    if (this.intervalId !== null) {
+      return;
+    }
+
+    this.intervalId = setInterval(() => {
+      const currentTime = this.getCurrentFormattedTime();
+
+      this.alarmCollection.forEach(alarm => {
+        if (alarm.time === currentTime && alarm.canCall) {
+          alarm.canCall = false;
+          alarm.callback();
+        }
+      });
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  resetAllCalls() {
+    this.alarmCollection.forEach(alarm => {
+      alarm.canCall = true;
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
